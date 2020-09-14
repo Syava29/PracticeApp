2c93c0258be70806ac6c864e1d48bdfc33a994b2
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-09-14 20:48:14 +0300

    добавил файл

--- testApp.py
+++ testApp.py
@@ -1,6 +1,31 @@
 from docxtpl import DocxTemplate
+from tkinter import *
+from tkinter import scrolledtext
 
-doc = DocxTemplate("my_word_template.docx")
-context = { 'company_name' : "World company" }
-doc.render(context)
-doc.save("generated_doc.docx")
+
+""" Бизнес_логика приложения"""
+def clicked():
+    doc = DocxTemplate("RPD_test.docx")
+    context = {'prepod': "ФИО"}
+    doc.render(context)
+    doc.save("RPD_final.docx")
+
+
+
+
+""" Визуализация """
+
+window = Tk()
+window.title("Генератор Рабочей программы дисциплины")
+window.geometry('1500x500')
+
+text_box = Text()
+text_box.grid(column=1, row=1)
+
+btn = Button(window, text="Подобрать литературу", comand=clicked())
+btn.grid(column=8, row=7)
+
+
+
+window.mainloop()
+
