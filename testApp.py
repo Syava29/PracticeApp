b2e5d0f25cf62cb6af9805daf53e399d77381290
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-09-17 02:22:36 +0300

    minor update

--- testApp.py
+++ testApp.py
@@ -2,7 +2,7 @@ from docxtpl import DocxTemplate
 from tkinter import *
 from tkinter import scrolledtext
 from tkinter import ttk
-from tkinter import Tk, BOTH
+from tkinter import Tk, BOTH, Menu
 from tkinter.ttk import Frame, Button
 from tkinter import messagebox as mbox
 
@@ -15,7 +15,7 @@ def getText():
     context = {'prepod': prepod_get, 'discip': discip_get, 'type_ed_prog': type_ed_prog_get}
     doc.render(context)
     doc.save("RPD_final.docx")
-    main()
+
 
 
 
@@ -23,44 +23,10 @@ def deleteText():
     entry.delete(0, END)
 
 
-class Example(Frame):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.master.title("Всплывающие окна с уведомлениями")
-        self.pack()
-
-        error = Button(self, text="Ошибка", command=self.onError)
-        error.grid(padx=5, pady=5)
-        warning = Button(self, text="Предупреждение", command=self.onWarn)
-        warning.grid(row=1, column=0)
-        question = Button(self, text="Вопрос", command=self.onQuest)
-        question.grid(row=0, column=1)
-        inform = Button(self, text="Информация", command=self.onInfo)
-        inform.grid(row=1, column=1)
-
-    def onError(self):
-        mbox.showerror("Ошибка", "Не могу открыть файл")
-
-    def onWarn(self):
-        mbox.showwarning("Предупреждение", "Вызов устаревшей функции")
-
-    def onQuest(self):
-        mbox.askquestion("Вопрос", "Вы уверены, что хотите выйти?")
-
-    def onInfo(self):
-        mbox.showinfo("Информация", "Скачивание завершено")
-
-
-def main():
-    root = Tk()
-    ex = Example()
-    root.geometry("300x150+300+300")
-    root.mainloop()
-
+def newWindow():
+    window = Tk()
+    window.title("Генератор Рабочей программы дисциплины")
+    window.geometry('1000x500')
 
 
 
@@ -71,10 +37,10 @@ def main():
 
 window = Tk()
 window.title("Генератор Рабочей программы дисциплины")
-window.geometry('1500x500')
+window.geometry('600x600')
 
 frm_form = Frame(relief=SUNKEN, borderwidth=3)
-frm_form.pack()
+frm_form.pack(fill=X, ipadx=5, ipady=5, padx=10)
 
 labels = [
     "ФИО:",
@@ -98,7 +64,7 @@ label_5 = Label(master=frm_form, text=labels[5])
 entry = Entry(master=frm_form, width=50)
 #entry_1 = Entry(master=frm_form, width=50)
 #entry_2 = Entry(master=frm_form, width=50)
-entry_3 = Entry(master=frm_form, width=50)
+#entry_3 = Entry(master=frm_form, width=50)
 entry_4 = Entry(master=frm_form, width=50)
 entry_5 = Entry(master=frm_form, width=50)
 # Использует менеджер геометрии grid для размещения ярлыков и
@@ -113,7 +79,7 @@ label_5.grid(row=6, column=0, sticky="e")
 entry.grid(row=1, column=1)
 #entry_1.grid(row=2, column=1)
 #entry_2.grid(row=3, column=1)
-entry_3.grid(row=4, column=1)
+#entry_3.grid(row=4, column=1)
 entry_4.grid(row=5, column=1)
 entry_5.grid(row=6, column=1)
 
@@ -126,18 +92,27 @@ combobox_discip.grid(row=2, column=1)
 
 combobox_ed_prog = ttk.Combobox(frm_form, values=[
                                     "бакалавриат",
-                                    "специалитет, магистратура",])
+                                    "специалитет, магистратура"])
 combobox_ed_prog.grid(row=3, column=1)
+
+combobox_form_ed = ttk.Combobox(frm_form, values=[
+                                    "очная",
+                                    "очно-заочная",
+                                    "заочная"])
+combobox_form_ed.grid(row=4, column=1)
 
 
 frm_buttons = Frame()
 frm_buttons.pack(fill=X, ipadx=5, ipady=5)
 
 btn_submit = Button(master=frm_buttons, text="Добавить", command=getText)
-btn_submit.pack(side=RIGHT, padx=10, ipadx=10)
+btn_submit.pack(side=LEFT, padx=10, ipadx=10)
 
 btn_clear = Button(master=frm_buttons, text="Отчистить", command=deleteText)
-btn_clear.pack(side=RIGHT, ipadx=10)
+btn_clear.pack(side=LEFT, padx=10, ipadx=10)
+
+btn_nw = Button(master=frm_buttons, text="New", command=newWindow)
+btn_nw.pack(side=LEFT, padx=10, ipadx=10)
 
 
 
