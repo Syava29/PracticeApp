c61ee59848d032f21878093641bfa40ac98ed2d7
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-09-18 03:00:29 +0300

    Created a menu

--- testApp.py
+++ testApp.py
@@ -1,12 +1,13 @@
 from docxtpl import DocxTemplate
 from tkinter import *
-from tkinter import scrolledtext
-from tkinter import ttk
-from tkinter import Tk, BOTH, Menu
+from tkinter import scrolledtext, Frame, Tk, BOTH, Text, Menu, END
+from tkinter import ttk, filedialog
 from tkinter.ttk import Frame, Button
 from tkinter import messagebox as mbox
 
 """ Бизнес_логика приложения"""
+
+
 def getText():
     prepod_get = entry.get()
     discip_get = combobox_discip.get()
@@ -15,8 +16,6 @@ def getText():
     context = {'prepod': prepod_get, 'discip': discip_get, 'type_ed_prog': type_ed_prog_get}
     doc.render(context)
     doc.save("RPD_final.docx")
-
-
 
 
 def deleteText():
@@ -29,18 +28,30 @@ def newWindow():
     window.geometry('1000x500')
 
 
-
-
+def onOpen():
+    ftypes = [('py файлы', '*.py'), ('Все файлы', '*')]
+    dlg = filedialog.Open(window, filetypes=ftypes)
+    fl = dlg.show()
+    fll = open(fl)
+    fl2 = fll.readline()
+    mainText.insert(1.0, fl2)
 
 
 """ Визуализация """
-
 window = Tk()
 window.title("Генератор Рабочей программы дисциплины")
 window.geometry('600x600')
 
+# создание меню
+menubar = Menu(window.master)
+window.config(menu=menubar)
+fileMenu = Menu(menubar)
+fileMenu.add_command(label="Открыть файл", command=onOpen)
+menubar.add_cascade(label="Файл", menu=fileMenu)
+
+# Создание Рамки с лэйблами и текстовыми полями
 frm_form = Frame(relief=SUNKEN, borderwidth=3)
-frm_form.pack(fill=X, ipadx=5, ipady=5, padx=10)
+frm_form.pack(fill=X, ipadx=30, ipady=30, padx=10)
 
 labels = [
     "ФИО:",
@@ -50,7 +61,6 @@ labels = [
     "Направление подготовки:",
     "Профиль:",
 ]
-
 
 # Создает ярлык с текстом из списка ярлыков.
 label = Label(master=frm_form, text=labels[0])
@@ -62,9 +72,9 @@ label_5 = Label(master=frm_form, text=labels[5])
 
 # Создает текстовое поле которая соответствует ярлыку.
 entry = Entry(master=frm_form, width=50)
-#entry_1 = Entry(master=frm_form, width=50)
-#entry_2 = Entry(master=frm_form, width=50)
-#entry_3 = Entry(master=frm_form, width=50)
+# entry_1 = Entry(master=frm_form, width=50)
+# entry_2 = Entry(master=frm_form, width=50)
+# entry_3 = Entry(master=frm_form, width=50)
 entry_4 = Entry(master=frm_form, width=50)
 entry_5 = Entry(master=frm_form, width=50)
 # Использует менеджер геометрии grid для размещения ярлыков и
@@ -77,30 +87,29 @@ label_4.grid(row=5, column=0, sticky="e")
 label_5.grid(row=6, column=0, sticky="e")
 
 entry.grid(row=1, column=1)
-#entry_1.grid(row=2, column=1)
-#entry_2.grid(row=3, column=1)
-#entry_3.grid(row=4, column=1)
+# entry_1.grid(row=2, column=1)
+# entry_2.grid(row=3, column=1)
+# entry_3.grid(row=4, column=1)
 entry_4.grid(row=5, column=1)
 entry_5.grid(row=6, column=1)
 
 combobox_discip = ttk.Combobox(frm_form, values=[
-                                    "Программирование",
-                                    "Философия",
-                                    "Эконометрика",
-                                    "Иностранный язык"])
+    "Программирование",
+    "Философия",
+    "Эконометрика",
+    "Иностранный язык"])
 combobox_discip.grid(row=2, column=1)
 
 combobox_ed_prog = ttk.Combobox(frm_form, values=[
-                                    "бакалавриат",
-                                    "специалитет, магистратура"])
+    "бакалавриат",
+    "специалитет, магистратура"])
 combobox_ed_prog.grid(row=3, column=1)
 
 combobox_form_ed = ttk.Combobox(frm_form, values=[
-                                    "очная",
-                                    "очно-заочная",
-                                    "заочная"])
+    "очная",
+    "очно-заочная",
+    "заочная"])
 combobox_form_ed.grid(row=4, column=1)
-
 
 frm_buttons = Frame()
 frm_buttons.pack(fill=X, ipadx=5, ipady=5)
@@ -114,13 +123,15 @@ btn_clear.pack(side=LEFT, padx=10, ipadx=10)
 btn_nw = Button(master=frm_buttons, text="New", command=newWindow)
 btn_nw.pack(side=LEFT, padx=10, ipadx=10)
 
+mainText = Text(window)
+mainText.pack()
 
+scroll = Scrollbar(command=mainText.yview)
+scroll.pack(side=LEFT, fill=Y)
 
-
+mainText.config(yscrollcommand=scroll.set)
 
 label = Label()
 label.pack(side=LEFT)
 
-
 window.mainloop()
-
