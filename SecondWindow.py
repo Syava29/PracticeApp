6fea9cc15618be081f27414592238a542457b1fb
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-09-28 15:24:18 +0300

    minor update

--- SecondWindow.py
+++ SecondWindow.py
@@ -10,34 +10,17 @@ from docxtpl import DocxTemplate
 class SecondWin:
     def __init__(self, parent):
         self.root = Toplevel(parent)
-        self.root.title("Генератор Рабочей программы дисциплины")
+        self.root.title("Генератор")
         self.root.geometry("600x600")
 
-    def create_menu(self):
-        menubar = Menu(self.master)
-        window.config(menu=menubar)
-
-        fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Открыть файл")
-        menubar_label = [
-            "Цели и задачи освоения дисциплины",
-            "Место дисциплины в структуре ОП",
-            "Планируемые результаты обучения по дисциплине",
-            "Структура дисциплины и распределение её трудоёмкости"
-        ]
-
-        description_menu_bar = Menu(menubar)
-        description_menu_bar.add_command(label="Цели и задачи освоения дисциплины")
-        description_menu_bar.add_command(label=menubar_label[0])
-
-
-        menubar.add_cascade(label="Файл", menu=fileMenu)
-        menubar.add_cascade(label="Содержание РПД", menu=description_menu_bar)
+    def labb(self):
+        Label(self.root, text='131313').pack
 
 
 
 
 if __name__ == "__main__":
     window = SecondWin(500, 500, "TKINTER")
-    SecondWin.create_menu()
     window.run()
+    SecondWin.create_menu()
+    SecondWin.labb()
