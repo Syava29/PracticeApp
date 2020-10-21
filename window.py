83d0778f08b07b3e1dbfc68f21ddda72e325c370
Andrey Sevostyanov <sevostyanovandrei@mail.ru>
2020-10-22 02:09:29 +0300

    update

--- window.py
+++ window.py
@@ -88,6 +88,8 @@ class MainWindow:
             "Форма обучения: *",
             "Направление подготовки: *",
             "Профиль: *",
+            "Инфститут(факультет)",
+            "Кафедра"
         ]
 
         # Создает ярлык с текстом из списка ярлыков.
@@ -97,8 +99,23 @@ class MainWindow:
         self.label_3 = Label(self.frm_form, text=self.labels[3])
         self.label_4 = Label(self.frm_form, text=self.labels[4])
         self.label_5 = Label(self.frm_form, text=self.labels[5])
-
+        self.label_inst_fac = Label(self.frm_form, text=self.labels[6])
+        self.label_kaf = Label(self.frm_form, text=self.labels[7])
         # Создает текстовое поле которая соответствует ярлыку.
+        self.combobox_inst_fac = ttk.Combobox(self.frm_form, width=50, values=[
+            "Физико-математический факультет",
+            "Институт естественных наук и биотехнологии",
+            "Юридический институт",
+            "Институт приборостроения, автоматизации и информационных технологий"], )
+
+        self.combobox_kaf = ttk.Combobox(self.frm_form, width=50, values=[
+            "Кафедра алгебры и математических методов в экономике",
+            "Кафедра геометрии и методики преподавания математики",
+            "Кафедра информатики",
+            "Кафедра математики и прикладных информационных технологий имени Н.А. Ильиной",
+            "Кафедра математического анализа и дифференциальных уравнений",
+            "Кафедра экспериментальной и теоретической физики"])
+
         self.entry = Entry(self.frm_form, width=50)
         # entry_1 = Entry(master=frm_form, width=50)
         # entry_2 = Entry(master=frm_form, width=50)
@@ -108,18 +125,17 @@ class MainWindow:
         # Использует менеджер геометрии grid для размещения ярлыков и
         # текстовых полей в строку, чей индекс равен idx.
         self.label.grid(row=1, column=0, sticky="e")
-        self.label_1.grid(row=2, column=0, sticky="e")
-        self.label_2.grid(row=3, column=0, sticky="e")
-        self.label_3.grid(row=4, column=0, sticky="e")
-        self.label_4.grid(row=5, column=0, sticky="e")
-        self.label_5.grid(row=6, column=0, sticky="e")
+        self.label_inst_fac.grid(row=2, column=0, sticky="e")
+        self.label_kaf.grid(row=3, column=0, sticky="e")
+        self.label_1.grid(row=4, column=0, sticky="e")
+        self.label_2.grid(row=5, column=0, sticky="e")
+        self.label_3.grid(row=6, column=0, sticky="e")
+        self.label_4.grid(row=7, column=0, sticky="e")
+        self.label_5.grid(row=8, column=0, sticky="e")
 
         self.entry.grid(row=1, column=1, pady=10)
-        # entry_1.grid(row=2, column=1)
-        # entry_2.grid(row=3, column=1)
-        # entry_3.grid(row=4, column=1)
-        # self.entry_4.grid(row=5, column=1)
-        # self.entry_5.grid(row=6, column=1)
+        self.combobox_inst_fac.grid(row=2, column=1, pady=10)
+        self.combobox_kaf.grid(row=3, column=1, pady=10)
 
         self.button_authorization = Button(self.frm_form, text='Вход', width=5, command=self.authorization).grid(row=1,
                                                                                                                  column=3)
@@ -128,26 +144,26 @@ class MainWindow:
             "Программирование",
             "Философия",
             "Эконометрика",
-            "Иностранный язык"], )
-        self.combobox_discip.grid(row=2, column=1, pady=10)
+            "Иностранный язык"])
+        self.combobox_discip.grid(row=4, column=1, pady=10)
 
         self.combobox_ed_prog = ttk.Combobox(self.frm_form, width=50, values=[
             "бакалавриат",
             "специалитет, магистратура"])
-        self.combobox_ed_prog.grid(row=3, column=1, pady=10)
+        self.combobox_ed_prog.grid(row=5, column=1, pady=10)
 
         self.combobox_form_ed = ttk.Combobox(self.frm_form, width=50, values=[
             "очная",
             "очно-заочная",
             "заочная"])
-        self.combobox_form_ed.grid(row=4, column=1, pady=10)
+        self.combobox_form_ed.grid(row=6, column=1, pady=10)
 
         self.combobox_naprav_podgotovki = ttk.Combobox(self.frm_form, width=50,
                                                        values=["09.03.03 Прикладная информатика"])
-        self.combobox_naprav_podgotovki.grid(row=5, column=1, pady=10)
+        self.combobox_naprav_podgotovki.grid(row=7, column=1, pady=10)
 
         self.combobox_profil = ttk.Combobox(self.frm_form, width=50, values=["Интеллектуальная обработка данных"])
-        self.combobox_profil.grid(row=6, column=1, pady=10)
+        self.combobox_profil.grid(row=8, column=1, pady=10)
 
         # основные кнопки
         self.frm_buttons = Frame()
@@ -245,13 +261,17 @@ class MainWindow:
         form_ed = self.combobox_form_ed.get()
         naprav_podgotovki = self.combobox_naprav_podgotovki.get()
         profil = self.combobox_profil.get()
-
+        inst_fac = self.combobox_inst_fac.get()
+        kaf = self.combobox_kaf.get()
         doc = DocxTemplate("data/RPD_test.docx")
         context = {'prepod': prepod_get, 'discip': discip_get, 'type_ed_prog': stype_ed_prog_get, 'form_ed': form_ed,
-                   'profil': profil, 'naprav_podgotovki': naprav_podgotovki, 'mesto_discip': "{{mesto_discip}}",
-                   'task': "{{task}}",
+                   'profil': profil, 'naprav_podgotovki': naprav_podgotovki, 'inst_fac': inst_fac,
+                   'kafedra': kaf, 'mesto_discip': "{{mesto_discip}}", 'task': "{{task}}",
                    'target': "{{target}}", 'description1': "{{description1}}", 'kod_komp1': "{{kod_komp1}}",
-                   'description2': "{{description2}}", 'kod_komp2': "{{kod_komp2}}"}
+                   'description2': "{{description2}}", 'kod_komp2': "{{kod_komp2}}",
+                   'zuv1_1': "{{zuv1_1}}", 'zuv1_2': "{{zuv1_2}}", 'zuv1_3': "{{zuv1_3}}",
+                   'zuv2_1': "{{zuv2_1}}", 'zuv2_2': "{{zuv2_2}}", 'zuv2_3': "{{zuv2_3}}"
+                   }
         doc.render(context)
         doc.save("data/RPD1.docx")
 
@@ -301,14 +321,12 @@ class MainWindow:
                 self.entry.delete(0, END)
                 self.entry.insert(0, string_test[0])
 
-                self.cur.execute(f'SELECT discip FROM discip WHERE id_prepod == {k+1}')
+                self.cur.execute(f'SELECT discip FROM discip WHERE id_prepod == {k + 1}')
                 all_results = self.cur.fetchall()
 
                 while i < len(all_results):
                     self.combobox_discip["values"] = all_results
                     i += 1
-                self.combobox_profil.insert(0, len(all_results))
-
                 mb.showinfo("Успешно", "Вход выполнен успешно")
                 flag = True
 
